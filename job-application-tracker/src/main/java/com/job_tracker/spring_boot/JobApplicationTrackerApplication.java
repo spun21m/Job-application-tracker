2fcc35f7a92cb033e28b0896f1a603dc1f59bef1
[Sital Pun] <[shitalmagar21@gmail.com]>
2026-01-09 19:46:39 -0500

    Spring boot prpject setup

---
+++ job-application-tracker/src/main/java/com/job_tracker/spring_boot/JobApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.job_tracker.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationTrackerApplication.class, args);
+	}
+
+}
---
+++ job-application-tracker/src/test/java/com/job_tracker/spring_boot/JobApplicationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_tracker.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
