a58dbb1846a63e92a0f6a2d794b7dcc3c81e4e32
[Sital Pun] <[shitalmagar21@gmail.com]>
2026-04-28 20:24:34 -0400

    Search and status filter added

--- job-application-tracker/frontend/src/pages/Home.jsx
+++ job-application-tracker/frontend/src/pages/Home.jsx
@@ -7,7 +7,23 @@ import "../styles/home.css";
 export default function Home() {
   const [applications, setApplications] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchApplication, setSearchApplication] = useState("");
+  const [statusFilter, setStatusFilter] = useState("ALL");
+  
+  const filteredApplication=applications.filter((app) =>{
 
+    console.log(applications[0]);
+    const searchText = searchApplication.toLowerCase().trim();
+    const matchesSearch =
+    (app.jobTitle || "").toLowerCase().trim().includes(searchText) ||
+    (app.companyName || "").toLowerCase().trim().includes(searchText);
+
+    const matchesStatus = 
+    statusFilter === "ALL" || app.status === statusFilter;
+    return matchesSearch && matchesStatus;
+});
+
+  
   useEffect(() => {
     fetchApplications();
   }, []);
@@ -45,12 +61,44 @@ export default function Home() {
         <p>💡 Track your job applications and interviews in one place.</p>
       </div>
       <div className="container">
+           
         <h2>Your Applications</h2>
-        <p>Total applications: {applications.length}</p>
+        {/* <p>Total applications: {applications.length}</p> */}
+        <p>Showing {filteredApplication.length} of {applications.length} applications</p>
+        <div>
+          <input
+            type="text"
+            placeholder="Search by job title or company..."
+            value={searchApplication}
+            onChange={(e) => setSearchApplication(e.target.value)}
+            className="search-input"
+          />
+        {searchApplication && (
+          <button
+            onClick={() => setSearchApplication("")}
+            className="clear"
+          >
+            Clear
+          </button>
+        )}
+        </div>
+        <select
+        value={statusFilter}
+        onChange={(e) => setStatusFilter(e.target.value)}
+        className="status-filter"
+        >
+          <option value="ALL">All</option>
+          <option value="APPLIED">Applied</option>
+          <option value="INTERVIEW">Interview</option>
+          <option value="OFFERED">Offered</option>
+          <option value="REJECTED">Rejected</option>
+
+
+        </select>
         {applications.length === 0 ? (
           <p>No applications found. Start by adding your first application!</p>
         ) : (
-          <JobList applications={applications} />
+          <JobList applications={filteredApplication} />
         )}
         <div className="actions">
           <Link to="/add">
